db4c99fd08e4969114d2783a474d4844823890a3
haejujung <167273792+haejujung@users.noreply.github.com>
2025-12-22 17:49:26 +0900

    complete css

---
+++ html/js/text.js
@@ -0,0 +1,4 @@
+const spanEI = document.querySelector("main h2 span");
+const txtArr = ['web publisher', 'front-end develpoer', 'web ui designer', 'ux designer', 'back-end developer'];
+let index = 0;
+let currentTxt = txtArr[index].split("");
